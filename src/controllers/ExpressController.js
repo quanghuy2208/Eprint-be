607373dba8f5fa98a191c2c52901fa7f3edb2409
quanghuy2208 <2208huyquang@gmail.com>
2024-10-14 08:20:04 +0700

    Update ExpressController.js

--- src/controllers/ExpressController.js
+++ src/controllers/ExpressController.js
@@ -1,7 +1,6 @@
 const nodemailer = require('nodemailer');
 const redis = require('redis');
 
-// Tạo client Redis
 const redisClient = redis.createClient({
   url: 'redis://localhost:6379',
 });
@@ -18,13 +17,11 @@ const generateOtp = () => {
 // Hàm gửi OTP qua email
 const sendOtp = async (email) => {
   const otp = generateOtp();
-
-  // Gửi email
   const transporter = nodemailer.createTransport({
     service: 'gmail',
     auth: {
-      user: 'your-email@gmail.com', // Thay bằng email của bạn
-      pass: 'your-email-password', // Thay bằng mật khẩu của bạn
+      user: 'huy528797@gmail.com',
+      pass: 'oswd gqqz vzam hqlm',
     },
   });
 
@@ -37,8 +34,7 @@ const sendOtp = async (email) => {
 
   try {
     await transporter.sendMail(mailOptions);
-    // Lưu OTP vào Redis với thời gian hết hạn
-    await redisClient.set(email, otp, 'EX', 300); // Hết hạn sau 5 phút
+    await redisClient.set(email, otp, 'EX', 300);
     return { status: 'OK', message: 'Mã OTP đã được gửi vào email của bạn.' };
   } catch (error) {
     console.error('Có lỗi xảy ra khi gửi email:', error);
@@ -48,11 +44,9 @@ const sendOtp = async (email) => {
 
 // Hàm kiểm tra mã OTP
 const validateOtp = async (email, otp) => {
-  // Lấy mã OTP từ Redis
   const storedOtp = await redisClient.get(email);
 
   if (storedOtp && storedOtp === otp) {
-    // Xóa mã OTP sau khi xác thực thành công
     await redisClient.del(email);
     return { status: 'OK', message: 'Mã OTP hợp lệ!' };
   } else {
