d1444ae64750f0a7456813b0d9b18fb457de48ba
quanghuy2208 <2208huyquang@gmail.com>
2024-10-14 10:00:25 +0700

    Update ExpressController.js

--- src/controllers/ExpressController.js
+++ src/controllers/ExpressController.js
@@ -11,32 +11,38 @@ redisClient.on('error', (err) => {
 
 // Hàm tạo mã OTP, lưu vào Redis và gửi email OTP
 const generateOtp = async (email) => {
-  // console.log(email)
-
-  const otp = Math.floor(100000 + Math.random() * 900000).toString();
-  console.log(otp)
-  
-  // Thiết lập cấu hình gửi email
-  const transporter = nodemailer.createTransport({
-    service: 'gmail',
-    auth: {
-      user: 'huy528797@gmail.com',
-      pass: 'oswd gqqz vzam hqlm',
-    },
-  });
-
-  const mailOptions = {
-    from: 'huy528797@gmail.com',
-    to: 'huy528797@gmail.com',
-    subject: 'Mã OTP của bạn',
-    text: `Mã OTP của bạn là: ${otp}`,
-  };
+  if (!redisClient.isOpen) {
+    await redisClient.connect();
+  }
 
   try {
+    // console.log('Email nhận được:', email);
+
+    const otp = Math.floor(100000 + Math.random() * 900000).toString();
+    console.log('OTP đã được tạo:', otp);
+
+    const transporter = nodemailer.createTransport({
+      service: 'gmail',
+      auth: {
+        user: 'huy528797@gmail.com',
+        pass: 'oswd gqqz vzam hqlm',
+      },
+    });
+
+    const mailOptions = {
+      from: 'huy528797@gmail.com',
+      to: 'huy528797@gmail.com',
+      subject: 'Mã OTP của bạn',
+      text: `Mã OTP của bạn là: ${otp}`,
+    };
+
+    await ensureRedisConnected();
+
     await transporter.sendMail(mailOptions);
-
+    
     await redisClient.set(email, otp, 'EX', 300);
-
+    
+    console.log('OTP đã được gửi thành công và lưu vào Redis');
     return { status: 'OK', message: 'Mã OTP đã được gửi vào email của bạn.' };
   } catch (error) {
     console.error('Có lỗi xảy ra khi gửi email hoặc tạo OTP:', error);
