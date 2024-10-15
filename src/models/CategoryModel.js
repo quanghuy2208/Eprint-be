5cfb63c2221e0312f4feab261468a0b65228db40
quanghuy2208 <2208huyquang@gmail.com>
2024-10-15 11:46:39 +0700

    Update CategoryModel.js

--- src/models/CategoryModel.js
+++ src/models/CategoryModel.js
@@ -1,8 +1,17 @@
 const mongoose = require("mongoose");
 
-const uri = "mongodb+srv://eprint:Kds513T4NpsjM4Jw@cluster0.4syayai.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+// const uri = "mongodb+srv://eprint:Kds513T4NpsjM4Jw@cluster0.4syayai.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
-const client = new MongoClient(uri);
+// const client = new MongoClient(uri);
 
-const Category = mongoose.model("Category", client);
+const categorySchema = new mongoose.Schema(
+    {
+      name: { type: String },
+    },
+    {
+      timestamps: true,
+    }
+  );
+
+const Category = mongoose.model("Category", categorySchema);
 module.exports = Category;
