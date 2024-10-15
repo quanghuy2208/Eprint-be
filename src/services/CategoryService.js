4bda3d2c4d098e369a36555497394b318a13ae6d
quanghuy2208 <2208huyquang@gmail.com>
2024-10-15 16:56:54 +0700

    Update CategoryService.js

--- src/services/CategoryService.js
+++ src/services/CategoryService.js
@@ -1,27 +1,11 @@
 const CategoryProduct = require("../models/CategoryModel.js");
 const bcrypt = require("bcrypt");
-// const { genneralAccessToken, genneralRefreshToken } = require("./JwtService");
-
-// async function getAllCategorylv1() {
-//     return new Promise(async (resolve, reject) => {
-//       try {
-//         const allCategorylv1 = await CategoryProduct.find().sort({ createdAt: -1, updatedAt: -1 });
-//         resolve({
-//           status: "OK",
-//           message: "Success",
-//           data: allCategorylv1,
-//         });
-//       } catch (e) {
-//         console.error("Error fetching categories:", e);
-//         reject(e);
-//       }
-//     });
-//   }
 
   const getAllCategorylv1 = () => {
     return new Promise(async (resolve, reject) => {
-        console.log("aaaaaaaaaaaaaaaaaaaaaaaa")
+
       try {
+        console.log("aaaaaaaaaaaaaaaaaaaaaaaa")
         const allCategorylv1 = await CategoryProduct.find().sort({ createdAt: -1, updatedAt: -1 });
         resolve({
           status: "OK",
