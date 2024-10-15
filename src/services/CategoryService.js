59ec5b29e6ab7c2b7a3f92c0399a1d2c8450518d
quanghuy2208 <2208huyquang@gmail.com>
2024-10-15 15:51:36 +0700

    Update CategoryService.js

--- src/services/CategoryService.js
+++ src/services/CategoryService.js
@@ -2,21 +2,37 @@ const CategoryProduct = require("../models/CategoryModel.js");
 const bcrypt = require("bcrypt");
 // const { genneralAccessToken, genneralRefreshToken } = require("./JwtService");
 
-async function getAllCategorylv1() {
+// async function getAllCategorylv1() {
+//     return new Promise(async (resolve, reject) => {
+//       try {
+//         const allCategorylv1 = await CategoryProduct.find().sort({ createdAt: -1, updatedAt: -1 });
+//         resolve({
+//           status: "OK",
+//           message: "Success",
+//           data: allCategorylv1,
+//         });
+//       } catch (e) {
+//         console.error("Error fetching categories:", e);
+//         reject(e);
+//       }
+//     });
+//   }
+
+  const getAllCategorylv1 = () => {
     return new Promise(async (resolve, reject) => {
       try {
         const allCategorylv1 = await CategoryProduct.find().sort({ createdAt: -1, updatedAt: -1 });
         resolve({
           status: "OK",
           message: "Success",
-          data: allCategorylv1, // Trả về dữ liệu
+          data: allCategorylv1,
         });
       } catch (e) {
-        console.error("Error fetching categories:", e); // Ghi lại lỗi
         reject(e);
       }
     });
-  }
+  };
+  
 
 module.exports = {
     getAllCategorylv1,
