f02540857b1dcaaac11a616a078061db2f27ac60
quanghuy2208 <2208huyquang@gmail.com>
2024-10-15 15:07:25 +0700

    ư

--- src/models/CategoryModel.js
+++ src/models/CategoryModel.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const categorySchema = new mongoose.Schema(
     {
-      name: { type: String },
+      name: { type: String, required: true, unique: true },
     },
     {
       timestamps: true,
@@ -12,3 +12,13 @@ const categorySchema = new mongoose.Schema(
 const Category = mongoose.model("Category", categorySchema);
 
 module.exports = Category;
+
+
+// const { MongoClient, ObjectId } = require('mongodb');
+
+// const uri = "mongodb+srv://eprint:Kds513T4NpsjM4Jw@cluster0.4syayai.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+// const client = new MongoClient(uri);
+
+// module.exports = client;
+
