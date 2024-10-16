bec24c2fa4ef4663c54366604c2402772a2c8274
quanghuy2208 <2208huyquang@gmail.com>
2024-10-16 09:45:57 +0700

    Update CategoryModel.js

--- src/models/CategoryModel.js
+++ src/models/CategoryModel.js
@@ -10,7 +10,7 @@ const categoryProductSchema = new mongoose.Schema(
   );
 
   const CategoryProduct = mongoose.model("CategoryProduct", categoryProductSchema);
-  console.log('bbbbbbbbbb')
+  console.log(CategoryProduct)
 
 module.exports = CategoryProduct;
 
