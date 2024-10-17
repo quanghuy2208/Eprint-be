4dac94d165507746ae0373e61755fe4a7899d87a
quanghuy2208 <2208huyquang@gmail.com>
2024-10-17 11:39:02 +0700

    Update UserController.js

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -50,7 +50,7 @@ const loginUser = async (req, res) => {
         message: "The input is email",
       });
     }
-    console.log(UserService.loginUser)
+    console.log(UserService.loginUser(req.body))
     const response = await UserService.loginUser(req.body);
 
     const { refresh_token, ...newReponse } = response;
