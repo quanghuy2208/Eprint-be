467a57a5d4c92048a7410a71a170bd0bdf73e8ba
quanghuy2208 <2208huyquang@gmail.com>
2024-10-24 09:01:33 +0700

    Update CardsService.js

--- src/services/CardsService.js
+++ src/services/CardsService.js
@@ -34,7 +34,7 @@ const getAllCard = () => {
   };
 
   const updateCard = async (cartId, productId, quantity) => {
-console.log("aaaaaaaaaaaaaa")
+console.log(productId)
     try {
 
       const cart = await Cards.findById(cartId);
