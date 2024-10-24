d6f1a03f7a43a819973367c50d98aed6fed8e15f
quanghuy2208 <2208huyquang@gmail.com>
2024-10-24 13:55:20 +0700

    Update CardsRouter.js

--- src/routes/CardsRouter.js
+++ src/routes/CardsRouter.js
@@ -10,7 +10,7 @@ router.get("/getAllCards", CardsController.getAllCards);
 router.get("/getCardsUser", CardsController.getCardsUser);
 router.put("/updateCard/:id", CardsController.updateCard);
 router.delete("/deleteCard/:id", CardsController.deleteCard);
-router.post("/addToCard/:id", CardsController.addToCard);
+router.post("/addToCard", CardsController.addToCard);
 
 router.post("/", (req, res) => res.send("ok"));
 
